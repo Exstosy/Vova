a57ea19d6702912de1704bb085aecc1d4d15f0dd
Exstosy <47812846+Exstosy@users.noreply.github.com>
2022-12-14 20:47:25 +0200

    Add files via upload

---
+++ 21Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter numbers:");
+      int count = 0;
+      string str = Console.ReadLine();
+      string[] strings = str.Split(",");
+      foreach (var s in strings){
+        if(Convert.ToInt32(s) > 0){
+          count++;
+        }
+      }
+      Console.WriteLine("Counter: {0}", count);
