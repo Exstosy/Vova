80fd0904f4aff3e58f03fbe08723946279ca4d85
Exstosy <47812846+Exstosy@users.noreply.github.com>
2022-12-14 20:13:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число b1");
+ float b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k1 ");
+ float k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b2");
+ float b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k2");
+ 
+  float k2 = Convert.ToInt32(Console.ReadLine());
+   float x = (-b2 + b1)/(-k1 + k2);
+   float y = (k2 * x+ b2);
+Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
